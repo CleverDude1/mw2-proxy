7ef94b82c24655d2e205adf57aa8f7561ef19d6b
CleverDude1 <212172494+CleverDude1@users.noreply.github.com>
2026-04-03 15:04:43 -0500

    Update users.js

--- api/users.js
+++ api/users.js
@@ -1,11 +1,8 @@
-
 export default async function handler(req, res) {
-  // Allow CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-  // Handle preflight
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
@@ -17,11 +14,10 @@ export default async function handler(req, res) {
         'Content-Type': 'application/x-www-form-urlencoded',
         'Authorization': 'Bearer a6cc01d0-205e-45cc-8550-d06c9c721e66'
       },
-      body: 'page=1&limit=2000'
+      body: 'page=1&limit=1000'
     });
 
     const data = await response.text();
-
     res.status(200).send(data);
 
   } catch (error) {

627eb3773b631f0fb1dab64334a97f63c853ffa8
CleverDude1 <212172494+CleverDude1@users.noreply.github.com>
2026-04-02 20:40:26 -0500

    Update users.js

--- api/users.js
+++ api/users.js
@@ -10,13 +10,14 @@ export default async function handler(req, res) {
   }
 
   try {
+    // Send POST request without limit
     const response = await fetch('https://martiangames.com/api/users', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
         'Authorization': 'Bearer a6cc01d0-205e-45cc-8550-d06c9c721e66'
       },
-      body: 'page=1&limit=1000'
+      body: 'page=1' // removed limit
     });
 
     const data = await response.text();
